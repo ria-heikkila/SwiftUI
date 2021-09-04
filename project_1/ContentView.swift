d0fc8c1eb088aa57522dc08f2dca343d2789e04c
Valeria Heikkila <valeria.heikkila@gmail.com>
2021-09-04 17:48:56 +0900

    Initial Commit

---
+++ project_1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  project_1
+//
+//  Created by Valeria Heikkila on 2021/09/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ project_1/project_1App.swift
@@ -0,0 +1,17 @@
+//
+//  project_1App.swift
+//  project_1
+//
+//  Created by Valeria Heikkila on 2021/09/04.
+//
+
+import SwiftUI
+
+@main
+struct project_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
